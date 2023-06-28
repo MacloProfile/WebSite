85406a1885f2d1d3c903ef56445a58c4d5e13f24
Denis <mr.lobzuk@gmail.com>
2023-06-28 11:00:22 +0300

    created

---
+++ src/main/java/com/example/webpastebinspringboot/WebPastebinSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.webpastebinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPastebinSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPastebinSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webpastebinspringboot/WebPastebinSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webpastebinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPastebinSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
