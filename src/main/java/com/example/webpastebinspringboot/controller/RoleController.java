caf752f13589cda9748b59529d96c1155094c6e8
Denis K <mr.lobzuk@gmail.com>
2023-07-13 15:46:41 +0300

    added profile tab with the ability to edit the password

--- src/main/java/com/example/webpastebinspringboot/controller/RoleController.java
+++ src/main/java/com/example/webpastebinspringboot/controller/RoleController.java
@@ -2,17 +2,16 @@ package com.example.webpastebinspringboot.controller;
 
 import com.example.webpastebinspringboot.domain.Role;
 import com.example.webpastebinspringboot.domain.User;
-import com.example.webpastebinspringboot.repositories.UserRepository;
+import com.example.webpastebinspringboot.service.UserService;
 import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 //common path
@@ -20,15 +19,17 @@ import java.util.stream.Collectors;
 public class RoleController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", userService.findAll());
 
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEdit(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
@@ -37,27 +38,31 @@ public class RoleController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(@RequestParam("userId") User user,
-                           @RequestParam Map<String,String> form,
+                           @RequestParam Map<String, String> form,
                            @RequestParam String username) {
-        user.setUsername(username);
-        //from ENUM to set<String>
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        //check that the form contains user roles
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepository.save(user);
+        userService.saveUser(user, username, form);
 
         return "redirect:/user";
+    }
+
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password
+    ) {
+        userService.updateProfile(user, password);
+
+        return "redirect:/user/profile";
     }
 }
--- src/main/java/com/example/webpastebinspringboot/service/UserService.java
+++ src/main/java/com/example/webpastebinspringboot/service/UserService.java
@@ -1,11 +1,20 @@
 package com.example.webpastebinspringboot.service;
 
+import com.example.webpastebinspringboot.domain.Role;
+import com.example.webpastebinspringboot.domain.User;
 import com.example.webpastebinspringboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -15,5 +24,37 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        //from ENUM to set<String>
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        //check that the form contains user roles
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepository.save(user);
+    }
+
+    //changes the password to his account
+    public void updateProfile(User user, String password) {
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userRepository.save(user);
     }
 }
